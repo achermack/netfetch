057bbc35da346df8337b98699f9f1ead9457858c
achermack <achermack@wisc.edu>
2021-12-30 00:08:55 -0600

    Solution restructure

---
+++ tests/netfetch.Tests/WinSystemInfoTests.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.Versioning;
+using netfetch;
+using Xunit;
+
+namespace netfetch.Tests;
+[SupportedOSPlatform("windows")]
+public class ISystemInfoTests
+{
+
+    [Fact]
+    public void Test_OS_Contains_Windows()
+    {
+        Assert.Contains("Windows", WinSystemInfo.OS);
+    }
+
+}
